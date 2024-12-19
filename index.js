efa27da319edad30071d157c242d5f20f49140e5
Prabhu kumar <prabhukumarmb@gmail.com>
2024-12-19 09:50:33 +0530

    installed package.json

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 const express = require("express")
+const dotenv = require("dotenv")
+dotenv.config()
+const app = express()
+
+
+let PORT = process.env.PORT
+console.log(PORT)
+app.listen(PORT, ()=>{
+    console.log(`listening at port ${PORT}`)
+})
