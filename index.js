5480a2df158b7114305af8fb87078a88609b0334
Prabhu kumar <prabhukumarmb@gmail.com>
2024-12-19 10:07:28 +0530

    created folder structure

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
-const express = require("express")
-const dotenv = require("dotenv")
+// const express = require("express")
+// const dotenv = require("dotenv")
+import express from "express"
+import dotenv from "dotenv"
+import cors from "cors"
+
 dotenv.config()
 const app = express()
 
+app.use(cors({
+    origin: "*"
+}))
+app.use(express.json())
 
+ 
 let PORT = process.env.PORT
-console.log(PORT)
 app.listen(PORT, ()=>{
-    console.log(`listening at port ${PORT}`)
+    console.log(`listening at port http://localhost:${PORT}`)
 })
