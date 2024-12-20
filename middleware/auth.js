5da0788cfab7559bdef9d6ba0e1fee63bb180d1e
Prabhu kumar <prabhukumarmb@gmail.com>
2024-12-20 20:14:00 +0530

    created auth file for users

---
+++ middleware/auth.js
@@ -0,0 +1,28 @@
+import jwt from "jsonwebtoken"
+import UserModel from "../models/usermodel"
+
+const authMiddleware = async (req, res, next)=>{
+    try{
+        let {token} = req.headers
+
+        if(!token){
+            throw new Error("Not authorized, Please login")
+        }
+
+        try{
+            let {_id} = jwt.verify(token ,process.env.JWT_SECREAT_KEY)
+            let user = await UserModel.findById(_id)
+            req.user = user
+            next()
+        }
+        catch(err){
+        res.status(400).json({msg: err.message, ok: false}) 
+        }
+
+    }
+    catch(err){
+        console.log(err)
+    }
+}
+
+export {authMiddleware};
--- routes/cartroutes.js
+++ routes/cartroutes.js
@@ -1,10 +1,11 @@
 import express  from 'express';
 import { addToCart, getCartItems, removeFromCart } from '../controllers/cartController';
+import { authMiddleware } from '../middleware/auth';
 
 const cartRouter = express.Router()
 
-cartRouter.post('/addtocart', addToCart)
-cartRouter.post('/removecartitem', removeFromCart)
-cartRouter.post('/getcartitems', getCartItems)
+cartRouter.post('/addtocart',  authMiddleware, addToCart)
+cartRouter.post('/removecartitem',  authMiddleware, removeFromCart)
+cartRouter.post('/getcartitems', authMiddleware,  getCartItems)
 
 export default cartRouter;
